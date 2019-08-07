bc1e254214d0d109448308808f458d81a0e854b5
Jeremic Arose <arosejeremic5@gmail.com>
2019-08-07 10:41:15 +0300

    updated sightings test

--- src/test/java/SightingsTest.java
+++ src/test/java/SightingsTest.java
@@ -1,2 +1,46 @@
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class SightingsTest {
+    @Rule
+    public DatabaseRule database = new DatabaseRule();
+
+    @Test
+    public void Sighting_InstantiatesCorrectly_True(){
+        Sightings newsighting = new Sightings("","",1);
+        assertTrue( newsighting instanceof Sightings);
+    }
+    @Test
+    public void Sighting_IsSavedOnDataBase_True(){
+        Sightings newSighting = new Sightings("","",1);
+        newSighting.save();
+        assertTrue(Sightings.all().get(0).equals(newSighting));
+    }
+    @Test
+    public void sighting_EachSigthingIsAssignedAnId_getid(){
+        Sightings newSighting = new Sightings("","",1);
+        newSighting.save();
+        Sightings testSighting = Sightings.all().get(0);
+        assertEquals(newSighting.getId(), testSighting.getId());
+    }
+    @Test
+    public void find_WillReturnSightingWithTheSameID_SecondSighting(){
+        Sightings firstSighting = new Sightings("","",1);
+        firstSighting.save();
+        Sightings SecondSighting = new Sightings("","",3);
+        SecondSighting.save();
+        assertEquals(SecondSighting, Sightings.find(SecondSighting.getId()));
+    }
+    @Test
+    public void Sightings_CanBeDeletedFromDataBase_true() {
+        Sightings newSighting = new Sightings("","",1);
+        newSighting.save();
+        newSighting.delete();
+        assertEquals(0, Sightings.all().size());
+    }
+
+
+
 }
