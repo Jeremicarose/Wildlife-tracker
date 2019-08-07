a9935145bcf14da4ac8d12b1fbf50e882e29fc26
Jeremic Arose <arosejeremic5@gmail.com>
2019-08-07 10:39:21 +0300

    updated databaserule

--- src/test/java/DatabaseRule.java
+++ src/test/java/DatabaseRule.java
@@ -1,2 +1,24 @@
-public class DatabaseRule {
+import org.junit.Test;
+import org.junit.rules.ExternalResource;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
+
+import static org.junit.Assert.assertEquals;
+
+public class DatabaseRule extends ExternalResource {
+    @Override
+    protected void before() {
+        Sql2o sql2otest = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "moringa", "1234");
+    }
+
+    @Override
+    protected void after() {
+        try (Connection con =  DB.sql2otest.open()) {
+            String deleteAnimalQuery = "DELETE FROM animal *;";
+            String deleteSightingQuery = "DELETE FROM sighting *;";
+            con.createQuery(deleteAnimalQuery).executeUpdate();
+            con.createQuery(deleteSightingQuery).executeUpdate();
+        }
+    }
 }
