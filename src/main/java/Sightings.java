537b55275486725feac8ee10e356faede92f11e7
Jeremic Arose <arosejeremic5@gmail.com>
2019-08-07 10:28:49 +0300

    updated sightings

--- src/main/java/Sightings.java
+++ src/main/java/Sightings.java
@@ -1,2 +1,44 @@
-public class Sightings {
+import org.sql2o.Connection;
+
+import java.util.List;
+
+public class Endangered extends  AnimalAbstract {
+    private static final String ANIMAL_TYPE = "endangered";
+    public Endangered(String name, String age, String health, String type) {
+        this.name = name;
+        this.age = age;
+        this.health = health;
+        this.type = ANIMAL_TYPE;
+    }
+    @Override
+    public boolean equals(Object otherAnimal){
+        if(!(otherAnimal instanceof Object)){
+            return false;
+        }
+        AnimalAbstract myAnimal = (AnimalAbstract) otherAnimal;
+        return this.getName().equals(myAnimal.getName())&&
+                this.getType().equals(myAnimal.getType())&&
+                this.getId()==myAnimal.getId() ;
+
+    }
+    @Override
+    public void save(){
+        try(Connection con = DB.sql2otest.open()){
+            String sql = "INSERT INTO animal (name, age, health, type) VALUES (:name, :age, :health, :type);";
+            this.id = (int) con.createQuery(sql, true)
+                    .addParameter("name", this.name)
+                    .addParameter("age", this.age)
+                    .addParameter("health", this.health)
+                    .addParameter("type", this.type)
+                    .executeUpdate()
+                    .getKey();
+        }
+    }
+    public static List<Endangered> all(){
+        String sql = "SELECT * FROM animal WHERE type='endangered'";
+        try(org.sql2o.Connection con = DB.sql2otest.open()) {
+            return con.createQuery(sql).executeAndFetch(Endangered.class);
+        }
+    }
+
 }
